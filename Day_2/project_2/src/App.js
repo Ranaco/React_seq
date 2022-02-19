70e7c8e9960bf9ad777284ebf887358eea5f113d
Ranaco <ranasatyamraj@gmail.com>
2022-02-19 14:45:06 +0530

    Learnt how react dom works and what exactly is react

---
+++ Day_2/project_2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import NavBar from './NavBar/NavBar';
+
+
+const App = () => {
+  return (
+    <div id = "base">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App;
---
+++ Day_2/project_2/src/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './NavBar.css';
+import logo from "../logo.svg";
+
+const NavBar = () => {
+    return(
+      <div id="header">
+          <div id = "head">
+             < logo />
+              React Js
+          </div>
+      </div>
+    )
+  }
+
+    export default NavBar;
